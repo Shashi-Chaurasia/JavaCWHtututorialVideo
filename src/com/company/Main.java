5c4f3ac628919cb0cdc40098c26d96727df7d6cb
Shashi_Chaurasia <shashichaurasia886@gmail.com>
2021-12-09 16:19:34 +0530

    first init

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 8;
+        int b = 67;
+        int c = 68;
+        int sum  = a + b+c;
+        System.out.println(sum);
+    }
+}

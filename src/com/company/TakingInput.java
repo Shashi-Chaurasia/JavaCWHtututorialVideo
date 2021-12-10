ef19b65297ac29e5ce97fcd71799f47e6b0a0d23
Shashi_Chaurasia <shashichaurasia886@gmail.com>
2021-12-10 14:24:17 +0530

    Percentage mark partice problem

--- src/com/company/TakingInput.java
+++ src/com/company/TakingInput.java
@@ -4,18 +4,39 @@ import java.util.Scanner;
 
 public class TakingInput {
     public static void main(String[] args) {
-        System.out.println("Enter first number");
+        /*System.out.println("Enter first number");
         Scanner sc = new Scanner(System.in);
         /* int a = sc.nextInt();
         System.out.println("Enter Second Number");
         int b = sc.nextInt();
         int sum = a + b;
-        System.out.println(sum);*/
+        System.out.println(sum);
 
         boolean b1 = sc.hasNextInt();
-        System.out.println(b1);
+        System.out.println(b1);*/
+
+
+        System.out.println("Enter your Subject Number : ");
+        System.out.print("Maths:");
+        Scanner sc = new Scanner(System.in);
+        int math = sc.nextInt();
+        System.out.print("English:");
+        int eng = sc.nextInt();
+        System.out.print("Hindi:");
+        int hindi = sc.nextInt();
+        System.out.print("Science:");
+        int science = sc.nextInt();
+        System.out.print("Social Science:");
+        int ss = sc.nextInt();
+
+        int sum = math + eng + hindi + ss + science;
+
+        float stuPercent = sum/5;
+        System.out.println("Percentage of student is : " + stuPercent);
     }
 }
+
+
 
 
 
